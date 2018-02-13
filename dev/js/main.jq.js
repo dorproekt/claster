77ec82bfc84582b4dc856e4dd393c5c511a1c9df
Alexandr Gorban <ukrgorban@gmail.com>
2018-02-13 17:04:20 +0200

    nav menu 1.0

--- dev/js/main.jq.js
+++ dev/js/main.jq.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     
+    //menu
+    let widthScreen = screen.width;
+    
+    $('nav li').hover(function(e){
+        if(e.clientX > widthScreen/2){// меню в правой части
+           $(this).children('.submenu').css({right: 0});
+        }else{// меню в левой части
+            $(this).children('.submenu').css({left: 0});
+        }
+        $(this).children('.submenu, .arrow-up').css('display','flex');
+    }, function(){
+        if($(this).hasClass('item')){
+            $('.submenu, .arrow-up').css('display','none');
+        }
+    });
+    
     //slider
     $('#slider').slick({
         dots: true,
