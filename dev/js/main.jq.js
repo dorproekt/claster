4b79f9a545ff4df78fcd88208e942ba1e53092f9
Alexandr Gorban <ukrgorban@gmail.com>
2018-02-15 15:30:03 +0200

    index.html beta

--- dev/js/main.jq.js
+++ dev/js/main.jq.js
@@ -25,5 +25,67 @@ $(document).ready(function(){
         prevArrow: '<img class="control prev" src="img/prev.png" alt=""/>',
         nextArrow: '<img class="control next" src="img/next.png" alt=""/>',
     });
-
-});
+    
+    //slider clients
+    $('#clients').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 2000,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            
+        ]
+    });
+});   
+    /*
+    
+                {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            });
+    
+    */
